e6d71fe61af47fca6004ffa04fcae84d9ac99484
Leandro Mariotti <midiasmart99@gmail.com>
2020-09-09 16:15:53 -0300

    Exxercicios Condicionais

--- Condicionais/src/Condition/Exercicio2.java
+++ Condicionais/src/Condition/Exercicio2.java
@@ -29,21 +29,21 @@ public class Exercicio2 {
 		
 		System.out.println();
 		if((num1 > num2) && (num1 > num3)) {
-			System.out.println(num1 + ", " + num2 + ", " + num3);
+			System.out.println(num3 + ", " + num2 + ", " + num1);
 			
 		}else if((num2 > num3) && (num3 > num1)) {
-			System.out.println(num2 + ", " + num3 + ", " + num1);
+			System.out.println(num1 + ", " + num3 + ", " + num2);
 			 
 		}else if((num3 > num1) && (num1 > num2)) {
 			System.out.println(num3 + ", " + num1 + ", " + num2);
 		}else if(((num3 == num1) && (num1 > num2))) {
-			System.out.println(num3 + ", " + num1 + ", " + num2);
+			System.out.println(num2 + ", " + num1 + ", " + num3);
 			
 		} else if(((num3 < num1) && (num1 == num2))) {
-			System.out.println(num1 + ", " + num2 + ", " + num3);
+			System.out.println(num3 + ", " + num2 + ", " + num1);
 			
 		}else {
-				System.out.println(num2 + ", " + num3 + ", " + num1);
+				System.out.println(num1 + ", " + num2 + ", " + num3);
 		}
 		
 		
